d9e2387a09d8a3cef48ed9df0a71cc0d6e5fea0a
bawarzec <bartlomiejwarzecha2@gmail.com>
2023-04-22 13:46:15 +0200

    Centralize configuration, add PDO data to class constructor, make bootstrap.php contain configuration variable

--- starShips/bootstrap.php
+++ starShips/bootstrap.php
@@ -4,5 +4,11 @@ require_once __DIR__.'/lib/BattleManager.php';
 require_once __DIR__.'/lib/BattleResult.php';
 require_once __DIR__.'/lib/ShipLoader.php';
 
+$configuration = array(
+    'db_dsn'  => 'mysql:host=localhost;dbname=oo_battle',
+    'db_user' => 'root',
+    'db_pass' => null,
+);
+
 ?>
 
--- starShips/lib/ShipLoader.php
+++ starShips/lib/ShipLoader.php
@@ -3,6 +3,16 @@
 
 class ShipLoader
 {
+    private $dbDsn;
+    private $dbUser;
+    private $dbPass;
+    public function __construct($dbDsn, $dbUser, $dbPass ){
+
+        $this->dbDsn = $dbDsn;
+        $this->dbUser = $dbUser;
+        $this->dbPass = $dbPass;
+
+    }
     private $pdo;
     /**
      * @return Ship[]
@@ -23,7 +33,7 @@ class ShipLoader
     private function getPDO()
     {
         if ($this->pdo === null) {
-            $this->pdo = new PDO('mysql:host=localhost;dbname=oo_battle', 'root');
+            $this->pdo = new PDO(this->$dbDsn, this->$dbUser,this->$dbPass);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         return $this->pdo;
